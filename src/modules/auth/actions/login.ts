b487f7ed59fe7e76005ea92997ebe7e3dc6654f8
Khoa <Khoanh008@gmail.com>
2025-01-08 09:32:55 +0700

    #0 feat: fix axios

--- src/modules/auth/actions/login.ts
+++ src/modules/auth/actions/login.ts
@@ -1,80 +1,67 @@
-/* eslint-disable prettier/prettier */
-'use server'
+// 'use server'
 
-import argon2 from 'argon2'
-import jwt, { Secret } from 'jsonwebtoken'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
+// import argon2 from 'argon2'
+// import jwt, { Secret } from 'jsonwebtoken'
+// import { cookies } from 'next/headers'
+// import { redirect } from 'next/navigation'
 
-import { config } from '@/lib/config'
-import { getSheets } from '@/lib/server/google-sheets'
-import { LoginSchema, loginSchema } from '@/modules/auth/schemas/login.schema'
+// import { config } from '@/lib/config'
+// import { getSheets } from '@/lib/server/google-sheets'
+// import { LoginSchema, loginSchema } from '@/modules/auth/schemas/login.schema'
 
-export const login = async (values: LoginSchema) => {
-	const validatedValues = loginSchema.safeParse(values)
+// export const login = async (values: LoginSchema) => {
+// 	const validatedValues = loginSchema.safeParse(values)
 
-	const coockieStore = await cookies()
+// 	const coockieStore = await cookies()
 
-	if (!validatedValues.success)
-		return {
-			error: validatedValues.error.message
-		}
+// 	if (!validatedValues.success)
+// 		return {
+// 			error: validatedValues.error.message
+// 		}
 
-	const body = validatedValues.data
+// 	const body = validatedValues.data
 
-	const sheets = await getSheets()
+// 	if (!users)
+// 		return {
+// 			error: 'Máy chủ đang gián đoạn, vui lòng thử lại sau'
+// 		}
 
-	const {
-		data: { values: users }
-	} = await sheets.spreadsheets.values.get({
-		spreadsheetId: config.SHEET_USER_ID,
-		range: config.SHEET_USER_NAME
-	})
+// 	const existingUserIdx = users.findIndex(
+// 		(row: string[]) =>
+// 			row[5] === body.phoneOrEmail || row[6] === body.phoneOrEmail
+// 	)
 
-	if (!users)
-		return {
-			error: 'Máy chủ đang gián đoạn, vui lòng thử lại sau'
-		}
+// 	if (existingUserIdx === -1)
+// 		return {
+// 			error: 'Thông tin đăng nhập không chính xác'
+// 		}
 
-	const existingUserIdx = users.findIndex(
-		(row: string[]) =>
-			row[5] === body.phoneOrEmail || row[6] === body.phoneOrEmail
-	)
+// 	const existingUser = users[existingUserIdx]
 
-	if (existingUserIdx === -1)
-		return {
-			error: 'Thông tin đăng nhập không chính xác'
-		}
+// 	const hashedPassword = existingUser[22]
 
-	const existingUser = users[existingUserIdx]
+// 	const verifyPassword = await argon2.verify(hashedPassword, body.password)
 
-	const hashedPassword = existingUser[22]
+// 	if (!verifyPassword)
+// 		return {
+// 			error: 'Thông tin đăng nhập chưa chính xác'
+// 		}
 
-	const verifyPassword = await argon2.verify(hashedPassword, body.password)
+// 	const accessToken = jwt.sign({
+// 		sub: existingUser[1],
+// 		publisherCode: existingUser[2],
+// 		managerCode: existingUser[3],
+// 		fullname: existingUser[4],
+// 		role: existingUser[23]
+// 	})
 
-	if (!verifyPassword)
-		return {
-			error: 'Thông tin đăng nhập chưa chính xác'
-		}
+// 	coockieStore.set('access-token', accessToken, {
+// 		httpOnly: true,
+// 		secure: true,
+// 		maxAge: 60 * 60 * 24 * 30,
+// 		sameSite: 'none',
+// 		path: '/'
+// 	})
 
-	const accessToken = jwt.sign(
-		{
-			sub: existingUser[1],
-			publisherCode: existingUser[2],
-			managerCode: existingUser[3],
-			fullname: existingUser[4],
-			role: existingUser[23]
-		},
-		config.ACCESS_TOKEN_SECRET as Secret
-	)
-
-	coockieStore.set('access-token', accessToken, {
-		httpOnly: true,
-		secure: true,
-		maxAge: 60 * 60 * 24 * 30,
-		sameSite: 'none',
-		path: '/'
-	})
-
-	redirect('/campaign')
-}
+// 	redirect('/campaign')
+// }
--- src/modules/auth/actions/register.ts
+++ src/modules/auth/actions/register.ts
@@ -1,28 +1,32 @@
 'use server'
 
-import { apiRequest } from '@/lib/http'
+import { AxiosError } from 'axios'
+
+import { instance } from '@/lib/http'
 import {
 	RegisterSchema,
 	registerSchema
 } from '@/modules/auth/schemas/register.shema'
 
-interface RegisterResponse {
-	verificationKey: string
-}
 export const register = async (values: RegisterSchema) => {
 	const validateData = registerSchema.safeParse(values)
 
-	if (!validateData.success)
-		return {
-			error: validateData.error.message
-		}
+	if (!validateData.success) {
+		return { error: validateData.error.message }
+	}
 
 	const body = validateData.data
 
-	const response = await apiRequest<RegisterResponse>(
-		'publishers/register',
-		body,
-		'POST'
-	)
-	console.log(response)
+	try {
+		const response = await instance.post('publishers/register', body)
+
+		console.log(response.data)
+
+		return { error: 'Verification key not found in response.' }
+	} catch (error) {
+		if (error instanceof AxiosError) console.error(error)
+		return {
+			error: error
+		}
+	}
 }
